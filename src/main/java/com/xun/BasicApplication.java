bd5dd80e571ef0750cad471466eec0dee726c15c
zhangzhe <zhangzhe_1518@163.com>
2019-05-25 23:15:56 +0800

    新建master分支

--- src/main/java/com/xun/BasicApplication.java
+++ src/main/java/com/xun/BasicApplication.java
@@ -3,6 +3,9 @@ package com.xun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhangzhe
+ */
 @SpringBootApplication
 public class BasicApplication {
 
