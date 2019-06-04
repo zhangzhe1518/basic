91003222b32d88b488adf9ff191954f9d60a5afe
zhangzhe <zhangzhe_1518@163.com>
2019-06-04 09:03:57 +0800

    多线程常用方法

--- src/main/java/com/xun/thread/UsuallyUsedMethods.java
+++ src/main/java/com/xun/thread/UsuallyUsedMethods.java
@@ -2,25 +2,27 @@ package com.xun.thread;
 
 /**
  * @author zhangzhe
+ * 经常使用的方法
  * Thread中的常用方法
  * 1.start()方法：启动当前线程，调用当前线程的run()方法
  * 2.run()：通常需要重写Thread中的此方法，将创建线程要执行的操作声明在此方法中
  * 3.CurrentThread()：是一个静态方法，返回执行当前代码的线程
  * 4.getName()：获取当前线程的名字
  * 5.setName()：设置当前线程的名字
- * 6.yidle()：释放当前cpu的执行权
+ * 6.yidle()：不会释放锁，并重新回到可执行状态
  * 7.join()：在线程A中调用线程B的join()方法，此时线程A就要进入阻塞状态等待线程B执行完以后继续执行抢夺cpu资源
- * 8.sleep()：使当前线程进入睡眠状态并让出cpu资源，指定时间到了继续抢夺cpu资源
+ * 8.sleep()：使当前线程进入睡眠状态不会释放锁，睡眠时间结束继续执行
  *
  * Object类的常用方法
  * 1.notify()：随机唤醒一个处于等待状态的线程
  * 2.notifyAll()：唤醒所有处于等待状态的线程
+ * 3.wait()：使当前线程进入阻塞状态并释放锁，使用notify()或notifyAll()方法唤醒
  *
  * 线程的优先级
  * 1.获取当前线程的优先级getPriority()
  * 2.设置当前线程的优先级setPriority()
  */
-public class ThreadMethods {
+public class UsuallyUsedMethods {
 
     public static void main(String[] args) {
         MyThread thread1 = new MyThread();
