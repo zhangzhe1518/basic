382afb48fc86edef337eb70903d9e622a4d3f57d
zhangzhe <zhangzhe_1518@163.com>
2019-06-15 23:08:16 +0800

    整合redis

--- src/test/java/com/xun/BasicApplicationTests.java
+++ src/test/java/com/xun/BasicApplicationTests.java
@@ -2,12 +2,28 @@ package com.xun;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BasicApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void test1(){
+        stringRedisTemplate.opsForValue().set("k1","寻。");
+        System.out.println(stringRedisTemplate.opsForValue().get("k1"));
+    }
+
 
     @Test
     public void contextLoads() {
