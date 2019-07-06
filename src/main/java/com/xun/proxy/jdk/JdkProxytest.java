9a2973ccfd73fad545dda543a8b2cdd3eb0e62a0
zhangzhe <zhangzhe_1518@163.com>
2019-07-07 05:46:28 +0800

    基于Jdk实现动态代理机制

--- src/main/java/com/xun/proxy/jdk/JdkProxytest.java
+++ src/main/java/com/xun/proxy/jdk/JdkProxytest.java
@@ -5,14 +5,15 @@ import java.lang.reflect.Proxy;
 public class JdkProxytest {
     public static void main(String[] args) {
 
+        //创建目标对象
         Person man = new Man();
-        //使用JDK提供的Proxy类下的newProxyInstance方法生成代理一个代理对象
+        //使用JDK提供的Proxy.newProxyInstance方法生成目标对象的代理对象
         Person person = (Person) Proxy.newProxyInstance(
-                //类加载器
+                //目标对象的类加载器
                 man.getClass().getClassLoader(),
-                //绑定的接口，也就是把代理对象绑定到哪些接口上，可以是多个
-                new Class[]{Person.class},
-                //绑定代理对象
+                //目标对象所实现的所有接口，可能是多个
+                man.getClass().getInterfaces(),
+                //绑定代理对象（代理对象的处理器）
                 new JdkProxyInvocation(man)
         );
 
